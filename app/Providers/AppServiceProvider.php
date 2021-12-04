7d4427ca799bcd59c8af402e507723f6abc05106
giljan <abellanosajangilbert@gmai.com>
2021-12-04 13:24:05 +0800

    initial release

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,10 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        If (env('APP_ENV') !== 'local') {
-            $this->app['request']->server->set('HTTPS', true);
+        if(config('app.env') === 'local') {
+            URL::forceScheme('https');
         }
+    }
+}
 
-        Schema::defaultStringLength(191);
-}
-}
